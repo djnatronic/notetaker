09da0efaba8445f0f74c0840941b8c1f251cdbf5
Nathan Bowdish <djnatronic@gmail.com>
2020-07-05 19:42:26 -0700

    id only thing not working

--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 var express = require("express");
 var path = require('path');
 var fs = require('fs');
+var bodyParser = require('body-parser')
+const data = require('./db/db.json')
+const util = require('util')
 
 // Set the port of our application
 // process.env.PORT lets the port be set by Heroku
@@ -10,16 +13,32 @@ var PORT = process.env.PORT || 3000;
 // Create express app instance.
 var app = express();
 app.use(express.static('public'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.get('/notes', function(req, res) {
     res.sendFile(path.join(__dirname + '/public/notes.html'));
 });
 
 app.get("/api/notes", function(req, res) {
-  const data = require('./db/db.json')
-  res.header("Content-Type",'application/json');
-  res.send(JSON.stringify(data));
+  res.send(data);
 
+});
+
+app.post("/api/notes", function(req, res) {
+  console.log(data)
+  data.push(req.body)
+  console.log(data)
+  res.send(data);
+});
+
+app.delete("/api/notes/:id", function(req, res) {
+  console.log("id is " + req.params.id);
+  data.splice(req.params.id,1)
+  console.log(data)
+  res.send(data);
 });
 
 app.get('*', function(req, res) {
